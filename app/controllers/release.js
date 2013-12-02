ce1dd51e98c8744b5b0d0509cbf879968dfd4946
Brad Sykes <bradleysykes1@gmail.com>
2013-12-02 03:23:26 -0500

    Added buttons for navigation, delete functionality

--- app/controllers/release.js
+++ app/controllers/release.js
@@ -9,6 +9,22 @@ exports.index = function(req, res, next) {
   });
 };
 
+exports.remove = function(req,res,next){
+  Release.remove({_id:req.param('_id')}, function(err,docs){
+    if(err)
+      return next(err)
+    res.redirect('/releases');
+  });
+};
+
+exports.removeAll = function(req,res,next){
+  Release.remove({},function(err,docs){
+    if(err)
+      return next(err)
+    res.redirect('/releases');
+  });
+};
+
 exports.new = function(req, res, next) {
   res.render('release/new');
 };
