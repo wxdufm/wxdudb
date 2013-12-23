6c7f583af298b4305cf1f9468e9eb5b746792d7a
Michael Lai <themichaellai@gmail.com>
2013-12-23 21:30:32 +0800

    Added failing Release tests

--- test/release/controller.js
+++ test/release/controller.js
@@ -4,6 +4,12 @@ var Release = require('../../app/models/release');
 var app = require('../../server');
 
 describe('Releases controller', function() {
+  var release = new Release({
+    artist: 'Neutral Milk Hotel',
+    title: 'In the Aeroplane Over the Sea',
+    status: 'PL'
+  });
+
   before(function(done) {
     Release.remove({}, done);
   });
@@ -16,9 +22,9 @@ describe('Releases controller', function() {
       request(app).post('/releases')
         .send({
           release: {
-            artist: "The Velvet Underground",
-            title: "The Velvet Underground & Nico",
-            status: "PL"
+            artist: release.artist,
+            title: release.title,
+            status: release.status
           }
         })
         .expect('Location', '/releases')
@@ -33,9 +39,60 @@ describe('Releases controller', function() {
     request(app).get('/releases')
       .expect(200)
       .end(function(err, res) {
-        res.text.should.include('The Velvet Underground');
-        res.text.should.include('PL');
+        res.text.should.include(release.title);
+        res.text.should.include(release.artist);
+        res.text.should.include(release.status);
         done();
       });
+  });
+
+  it('should show release detail on its page', function(done) {
+    request(app).get('/releases/' + release._id)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.text.should.include(release.title);
+        res.text.should.include(release.artist);
+        res.text.should.include(release.status);
+        done();
+      });
+  });
+
+  it('should show existing release info on edit page', function(done) {
+    request(app).get('/releases/' + release._id + '/edit')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.text.should.include(release.title);
+        res.text.should.include(release.artist);
+        res.text.should.include(release.status);
+        done();
+      });
+  });
+
+  it('should update release info', function(done) {
+    release.title = 'On Avery Island';
+    request(app).put('/releases/' + release._id)
+      .send({
+        release: {
+          title: release.title,
+        }
+      })
+      .expect('Location', '/releases')
+      .expect(302)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('should allow deleting of a release', function(done) {
+      request(app).del('/releases/' + release._id)
+        .expect('Location', '/releases')
+        .expect(302)
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        });
   });
 });
